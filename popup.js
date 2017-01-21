2e6c3e5c0c009f34034f007b116c79d79200c0c2
Tim van Steenbergen <tim@tieka.nl>
2017-01-21 09:59:25 +0100

    Creation of the git repo

---
+++ popup.js
@@ -0,0 +1,24 @@
+
+/**
+ * Created by Gebruiker on 21-1-2017.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('btn_login');
+    checkPageButton.addEventListener('click', function() {
+
+        chrome.tabs.getSelected(null, function(tab) {
+            d = document;
+
+            var f = d.createElement('form');
+            f.action = 'http://gtmetrix.com/analyze.html?bm';
+            f.method = 'post';
+            var i = d.createElement('input');
+            i.type = 'hidden';
+            i.name = 'url';
+            i.value = tab.url;
+            f.appendChild(i);
+            d.body.appendChild(f);
+            f.submit();
+        });
+    }, false);
+}, false);
