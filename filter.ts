dc820e35ebb0d69e2eb2e4ac53960f20ed3e0101
Tim van Steenbergen <tim@tieka.nl>
2017-03-10 06:48:24 +0100

    Attempt to decorate PasswordInputfield

--- filter.ts
+++ filter.ts
@@ -4,19 +4,17 @@
 // Google's bad word filter:
 // https://gist.githubusercontent.com/jamiew/1112488/raw/7ca9b1669e1c24b27c66174762cb04e14cf05aa7/google_twunter_lol
 
-// Runs getPasswordInputElements on the document
-function getPasswordInputElements():HTMLInputElement[]{
+// Runs decoratePasswordInputElements on the document
+function decoratePasswordInputElements():HTMLInputElement[]{
     let result = [];
     let inputs = document.getElementsByTagName("input");
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type.toLowerCase() === "password") {
-            alert(`found password inputfield on ${i}-th input-field`);
             // let decoratedElement = new HTMLDivElement();
             let decoratedElement = document.createElement('div');
-            decoratedElement.innerHTML = `${inputs[i]}`;
+            decoratedElement.innerHTML = inputs[i].outerHTML;
             decoratedElement.id = `OPFES_password_${i}`;
             inputs[i].parentNode.replaceChild(decoratedElement, inputs[i]);
-            alert('found & replaced password');
         }
     }
     return result;
@@ -27,12 +25,12 @@ function getPasswordInputElements():HTMLInputElement[]{
 let createObserver = function() {
     let observer = new MutationObserver((mutations) => {
             // Disconnect the MO so there isn't an infinite title update loop
-            // Run title getPasswordInputElements again
+            // Run title decoratePasswordInputElements again
             // Create a new MO to listen for more changes
             console.log('Mutations!', mutations);
     observer.disconnect();
     observer = null;
-    getPasswordInputElements;
+    decoratePasswordInputElements;
     createObserver()
 });
 
@@ -44,7 +42,7 @@ let createObserver = function() {
 createObserver();
 
 // Kick off initial page load check
-let passwordInputElements = getPasswordInputElements;
+let passwordInputElements = decoratePasswordInputElements;
 if (passwordInputElements().length >= 1){
     // nowWhat;
 }
