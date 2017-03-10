eb5d16fb589de601d92d604be29c15c818fc98a4
Tim van Steenbergen <tim.van.steenbergen@telfortglasvezel.nl>
2017-03-10 13:24:45 +0100

    First step in passwordfields-handling

--- filter.ts
+++ filter.ts
@@ -1,23 +1,22 @@
 /**
  * Created by tvansteenbergen on 2017-03-09.
  */
-// Google's bad word filter:
-// https://gist.githubusercontent.com/jamiew/1112488/raw/7ca9b1669e1c24b27c66174762cb04e14cf05aa7/google_twunter_lol
 
 // Runs decoratePasswordInputElements on the document
-function decoratePasswordInputElements():HTMLInputElement[]{
+function decoratePasswordInputElements(): HTMLInputElement[] {
     let result = [];
     let inputs = document.getElementsByTagName("input");
-    let pwdCounter:number = 0;
+    let pwdCounter: number = 0;
     for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].type.toLowerCase() === "password") {
+        if (inputs[i].type.toLowerCase() === `password`
+            && inputs[i].id.substring(0, 5) !== `OPFES`) {
             pwdCounter++;
-            let OPFES_PasswordInputElement:string = `<input id="OPFES_password${pwdCounter}" type="text"/>`;
-            let myImage:string = browser.extension.getURL("icons\/opfes_19.png");
-            let OPFES_MyImage:string = `<img name="OPFES_myImage" src="${myImage}"/>`;
+            let OPFES_PasswordInputElement: string = `<input id="OPFES_password${pwdCounter}" type="password"/>`;
+            let myImage: string = browser.extension.getURL("icons\/opfes_19.png");
+            let OPFES_MyImage: string = `<img name="OPFES_myImage" src="${myImage}"/>`;
 
             // let decoratedElement = new HTMLDivElement();
-            let decoratedElement = document.createElement('div');
+            let decoratedElement = document.createElement(`div`);
             decoratedElement.innerHTML = inputs[i].outerHTML + OPFES_PasswordInputElement + OPFES_MyImage;
             decoratedElement.id = `OPFES_password_${i}`;
             inputs[i].parentNode.replaceChild(decoratedElement, inputs[i]);
@@ -28,28 +27,28 @@ function decoratePasswordInputElements():HTMLInputElement[]{
 
 // Set up a mutation observer to listen for title changes
 // Will fire if framework AJAX stuff switches page title
-let createObserver = function() {
+let createObserver = function () {
     let observer = new MutationObserver((mutations) => {
-            // Disconnect the MO so there isn't an infinite title update loop
-            // Run title decoratePasswordInputElements again
-            // Create a new MO to listen for more changes
-            console.log('Mutations!', mutations);
-    observer.disconnect();
-    observer = null;
-    decoratePasswordInputElements;
-    createObserver()
-});
+        // Disconnect the MO so there isn't an infinite title update loop
+        // Run title decoratePasswordInputElements again
+        // Create a new MO to listen for more changes
+        console.log(`Mutations!`, mutations);
+        observer.disconnect();
+        observer = null;
+        decoratePasswordInputElements;
+        createObserver()
+    });
 
     observer.observe(
-        document.querySelector('title'),
-        { subtree: true, characterData: true, childList: true }
+        document.querySelector(`title`),
+        {subtree: true, characterData: true, childList: true}
     )
 };
 createObserver();
-// browser.extension.getURL('icons\/opfes_19.png');
+// browser.extension.getURL(`icons\/opfes_19.png`);
 
 // Kick off initial page load check
 let passwordInputElements = decoratePasswordInputElements;
-if (passwordInputElements().length >= 1){
+if (passwordInputElements().length >= 1) {
     // nowWhat;
 }
