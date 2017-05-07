109d53b144ed17be959991c26341aa043eead482
Tim van Steenbergen <tim@tieka.nl>
2017-05-07 21:31:15 +0200

    More unittests (but still want to see a lot more of them...)

--- SiteServiceTests.ts
+++ SiteServiceTests.ts
@@ -31,7 +31,7 @@
     testSites.push(new Site('testdomain.com', 'demosalt', 'demo@testdomain.com', 1, 119));
     expectedPasswords.push('.VTg!6gzs33Z/C@8S_bkdDF_49(raRd5-R%3/a?zdErK4u{4~6aT48edE(4mafSKANM(@E0e5FG4em@{9a_?bc3-w~H#WVHnmKZMpksSza)0s5KNdah@./h');
     testSites.push(new Site('testdomain.com', 'demosalt', 'demo@testdomain.com', 1, 120));
-    expectedPasswords.push('z~+8d.A?V0{e5Ww8Ra43Z:h=)_r?)~Uzew_U#65n96!bwkP)t@eswHEsSn5+UR30k.USmT#Rw^HK4p==-Vp!c46!:a{W}k=~Tn4eKh@nHmP#{NNVusK-WTeN');
+    expectedPasswords.push('y~+8d.A?V0{e5Ww8Ra43Z:h=)_r?)~Uzew_U#65n96!bwkP)t@eswHEsSn5+UR30k.USmT#Rw^HK4p==-Vp!c46!:a{W}k=~Tn4eKh@nHmP#{NNVusK-WTeN');
 
     for (let i = 0; i < testSites.length; i++) {
         let generatedPassword: string = SiteService.getSitePassword(testSites[i], 'demo');
