d7395b10d4898ff141412f1f6382577a5cf839c7
Tim van Steenbergen <tim@tieka.nl>
2017-06-07 07:42:31 +0200

    Improved layout, less mess on the toolbar form.

--- OnePasswordForEverySiteApp.js
+++ OnePasswordForEverySiteApp.js
@@ -30,13 +30,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // showTheLocallyStoredData(userData);
         toggleChangability.call(this);
     });
-    document.getElementById('OPFES_InputSaltToggle').addEventListener('click', function () {
-        toggleChangability.call(this);
-    });
     document.getElementById('OPFES_InputUserIdToggle').addEventListener('click', function () {
         toggleChangability.call(this);
     });
-    document.getElementById('OPFES_InputSequenceNrToggle').addEventListener('click', function () {
+    document.getElementById('OPFES_InputSaltToggle').addEventListener('click', function () {
+        toggleChangability.call(this);
+    });
+    document.getElementById('OPFES_SelectSequenceNrToggle').addEventListener('click', function () {
         toggleChangability.call(this);
     });
     document.getElementById('OPFES_SelectMaxPwdCharsToggle').addEventListener('click', function () {
@@ -48,21 +48,20 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('OPFES_InputRemarkToggle').addEventListener('click', function () {
         toggleChangability.call(this);
     });
-    document.getElementById('OPFES_InputAppPasswordShow').addEventListener('click', function () {
-        let elementId = this.id.substr(0, this.id.length - 4);
+    document.getElementById('OPFES_InputAppPasswordShowHide').addEventListener('click', function () {
+        let elementId = this.id.substr(0, this.id.length - 8);
         let elementToToggle = document.getElementById(elementId);
-        elementToToggle.setAttribute('type', 'text');
-        document.getElementById('OPFES_InputAppPasswordShow').setAttribute('disabled', 'disabled');
-        document.getElementById('OPFES_InputAppPasswordHide').removeAttribute('disabled');
-    });
-    document.getElementById('OPFES_InputAppPasswordHide').addEventListener('click', function () {
-        let elementToToggle = document.getElementById(this.id.substr(0, this.id.length - 4));
-        elementToToggle.setAttribute('type', 'password');
-        document.getElementById('OPFES_InputAppPasswordShow').removeAttribute('disabled');
-        document.getElementById('OPFES_InputAppPasswordHide').setAttribute('disabled', 'disabled');
+        if (this.value == 'Show') {
+            elementToToggle.setAttribute('type', 'text');
+            this.value = 'Hide ';
+        }
+        else {
+            elementToToggle.setAttribute('type', 'password');
+            this.value = 'Show';
+        }
     });
     /**
-     * This links the UserData-download function to the export button
+     * This links the UserData-upload function to the export button
      */
     document.getElementById("OPFES_CopyDiskToLocalStorageButton").addEventListener("change", function (event) {
         event.preventDefault();
@@ -109,17 +108,17 @@ let OPFES_WorkWithUserData = function (userData) {
                     if (site.getDomain() == domain) {
                         //Set the values of the other input-fields on the popup-screen
                         document.getElementById('OPFES_InputDomain').setAttribute('disabled', "disabled");
-                        if (site.getSalt() != "") {
-                            document.getElementById('OPFES_InputSalt').setAttribute('value', site.getSalt());
-                            document.getElementById('OPFES_InputSalt').setAttribute('disabled', "disabled");
-                        }
                         if (site.getUserId() != "") {
                             document.getElementById('OPFES_InputUserId').setAttribute('value', site.getUserId());
                             document.getElementById('OPFES_InputUserId').setAttribute('disabled', "disabled");
                         }
+                        if (site.getSalt() != "") {
+                            document.getElementById('OPFES_InputSalt').setAttribute('value', site.getSalt());
+                            document.getElementById('OPFES_InputSalt').setAttribute('disabled', "disabled");
+                        }
                         if (site.getSequenceNr() != 0) {
-                            document.getElementById('OPFES_InputSequenceNr').setAttribute('value', (site.getSequenceNr() + ""));
-                            document.getElementById('OPFES_InputSequenceNr').setAttribute('disabled', "disabled");
+                            document.getElementById('OPFES_SelectSequenceNr').setAttribute('value', (site.getSequenceNr() + ""));
+                            document.getElementById('OPFES_SelectSequenceNr').setAttribute('disabled', "disabled");
                         }
                         document.getElementById('OPFES_SelectMaxPwdChars').selectedIndex = site.getMaxPwdChars();
                         document.getElementById('OPFES_SelectMaxPwdChars').setAttribute('disabled', "disabled");
@@ -145,12 +144,12 @@ let OPFES_WorkWithUserData = function (userData) {
      */
     function showTheLocallyStoredData(userData, numOfSitesToShow = 999999) {
         let sites = userData.sites;
-        let dataTableHTML = "<table id='locallyStoredUserData' border='1px solid brown'><thead><td>domain</td><td>salt</td><td>userid</td><td>seq.nr</td><td>#chars</td><td>allowed</td><td>used at</td></ts><td>remark</td></thead>";
+        let dataTableHTML = "<table id='locallyStoredUserData' border='1px solid brown'><thead><td>domain</td><td>userid</td><td>salt</td><td>seq.nr</td><td>#chars</td><td>allowed</td><td>used at</td></ts><td>remark</td></thead>";
         let sitesToShow = sites.slice(0, numOfSitesToShow);
         for (let site of sitesToShow) {
             dataTableHTML += `<tr><td>${site.getDomain()}</td>
-                                 <td>${site.getSalt()}</td>
                                  <td>${site.getUserId()}</td>
+                                 <td>${site.getSalt()}</td>
                                  <td>${site.getSequenceNr()}</td>
                                  <td>${site.getMaxPwdChars()}</td>
                                  <td>${site.getAllowedSpecialCharacters()}</td>
@@ -173,13 +172,13 @@ let OPFES_WorkWithUserData = function (userData) {
     /* The functions for the buttons to Copy LocalStorage to disk and back again, (OPFES_CopyLocalStorageToDiskButton and
      * OPFES_CopyDiskToLocalStorageButton) are defined in their own files. */
     /*
-     * Upon clicking the loginButton, generate the password using the domain-name, salt, userId and sequenceNr,
+     * Upon clicking the loginButton, generate the password using the domain-name, userId, salt  and sequenceNr,
      * show the password in a popup and save the (changed) domain-data to the LocalStorage,
      * N.B. of course not saving the password!
      */
     document.getElementById('OPFES_LoginButton').addEventListener('click', function () {
         let ourPopup = document;
-        let site = new Site(ourPopup.getElementById('OPFES_InputDomain').value, ourPopup.getElementById('OPFES_InputSalt').value, ourPopup.getElementById('OPFES_InputUserId').value, +ourPopup.getElementById('OPFES_InputSequenceNr').value, +ourPopup.getElementById('OPFES_SelectMaxPwdChars').value, ourPopup.getElementById('OPFES_InputAllowedSpecialCharacters').value, new Date(Date.now()), ourPopup.getElementById('OPFES_InputRemark').value);
+        let site = new Site(ourPopup.getElementById('OPFES_InputDomain').value, ourPopup.getElementById('OPFES_InputUserId').value, ourPopup.getElementById('OPFES_InputSalt').value, +ourPopup.getElementById('OPFES_SelectSequenceNr').value, +ourPopup.getElementById('OPFES_SelectMaxPwdChars').value, ourPopup.getElementById('OPFES_InputAllowedSpecialCharacters').value, new Date(Date.now()), ourPopup.getElementById('OPFES_InputRemark').value);
         let inputValueAppPassword = ourPopup.getElementById('OPFES_InputAppPassword').value;
         //save the sites data every time the password gets generated
         // siteService.add(site)
@@ -195,7 +194,7 @@ let OPFES_WorkWithUserData = function (userData) {
         }
         userData.persist();
         let sitePassword = SiteService.getSitePassword(site, inputValueAppPassword);
-        window.prompt('The password for this site for this user-id is: ' + sitePassword + ' To copy the password to your clipboard: Ctrl+C, Enter', sitePassword);
+        window.alert('Your password for this site for this user-id is:\n\n' + sitePassword + '\n\n To copy the password to your clipboard: Ctrl+C, Enter');
         let passwordElement = ourPopup.getElementById('OPFES_InputSitePassword');
         passwordElement.setAttribute("value", sitePassword);
         // Insert the sitePassword in the password-input field in the document
