156caa70c2241cbcf03d9a96e5279f2fd6685033
Tim van Steenbergen <tim@tieka.nl>
2018-02-11 11:49:41 +0100

    Saving current websitefiles as localed on opfes.com jan2018

--- translations/en.ts
+++
@@ -1,6 +0,0 @@
-export const en = {
-    "OPFES_TITLE": "OPFES - One password for every site",
-    "COPY_FILE": "Copy File",
-    "DATA": "Data",
-    "DOWNLOAD_DATA": "This will copy the sites and their related properties to a file for you to store on your local drive"
-};
--- translations/fr.ts
+++
@@ -1,6 +0,0 @@
-export const fr = {
-    "OPFES_TITLE": "OPFES - Un mot de passe pour chaque site",
-    "COPY_FILE": "Copier un fichier",
-    "DATA": "Les données",
-    "DOWNLOAD_DATA": "xxCela copiera les sites et leurs propriétés connexes dans un fichier que vous devez stocker sur votre lecteur local"
-};
